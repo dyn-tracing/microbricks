2ed6949d2447d5a01e3dca4ef173a1417fa6f371
Muhammad Haseeb <mh6218@nyu.edu>
2022-11-26 14:45:07 -0500

    using my own microbricks in docker file

--- docker/bricks/start.sh
+++ docker/bricks/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./client -t ../config/two_topology.json -a ../config/two_addresses.json service1 > ./clogs.txt &
+./client -t ../config/two_topology.json -a ../config/two_addresses.json --openloop --requests=10 service1 > ./clogs.txt &
 
-./server -t ../config/two_topology.json -a ../config/two_addresses.json -x ot-jaeger service1 -h otelcollector -p 6832
+./server -t ../config/two_topology.json -a ../config/two_addresses.json -x ot-jaeger service1 -h otelcollector -p 6832 &
+
+./server -t ../config/two_topology.json -a ../config/two_addresses.json -x ot-jaeger service2 -h otelcollector -p 6832
